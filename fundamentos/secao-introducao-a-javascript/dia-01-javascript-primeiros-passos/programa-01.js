e40f6ce7852d082b7de568546a88729f660b9ca5
ricardokuhlkamp <ric.kds@hotmail.com>
2022-09-21 18:32:13 -0300

    programa-01

---
+++ fundamentos/secao-introducao-a-javascript/dia-01-javascript-primeiros-passos/programa-01.js
@@ -0,0 +1,22 @@
+/*
+Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)
+*/
+let a = 2;
+let b = 3;
+
+let adicao = a + b;
+let subtracao = (a - b);
+let multiplicacao = (a * b);
+let divisao = (a / b);
+let modulo = (a % b);
+
+console.log("adição: ", adicao);
+console.log("subtração: ", subtracao);
+console.log("multiplicação: ", multiplicacao);
+console.log("divisão: ", divisao);
+console.log("módulo: ", modulo);
