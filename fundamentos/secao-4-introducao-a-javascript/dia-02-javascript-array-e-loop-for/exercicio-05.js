35ff8dc2f6d269a08d6ed6179ad09e0cb1b842cf
ricardokuhlkamp <ric.kds@hotmail.com>
2022-09-22 19:43:36 -0300

    exercicio 06 resolvido

--- fundamentos/secao-4-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-05.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-05.js
@@ -6,7 +6,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maiorNumero = numbers[0];
 for (let index = 0; index < numbers.length; index++) {
   if (numbers[index] > maiorNumero) {
-    maiorNumero = numbers[index]
+    maiorNumero = numbers[index];
   }
   
   
