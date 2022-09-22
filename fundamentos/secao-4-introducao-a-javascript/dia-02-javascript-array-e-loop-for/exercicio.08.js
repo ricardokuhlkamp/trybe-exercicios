984c7fbe29547f7793639880472c704137d05515
ricardokuhlkamp <ric.kds@hotmail.com>
2022-09-22 20:36:01 -0300

    exercicio-08 resolvido

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio.08.js
@@ -0,0 +1,9 @@
+//Lidando com Arrays
+
+//Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+let valores =[];
+for (let index = 1; index <= 25; index++) {
+  valores.push(index);  
+}
+console.log(valores);
