408159048a0825c22d8577b491d4eeaaf9b7c97e
ricardokuhlkamp <ric.kds@hotmail.com>
2022-09-22 18:19:50 -0300

    exercicio 01 resolvido

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-01.js
@@ -0,0 +1,10 @@
+//Lidando com Arrays
+//Iremos utilizar esse array para realizar os próximos exercícios.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+for (let index = 0; index < numbers.length; index++) {
+  const element = numbers[index];
+  console.log(element);
+}
