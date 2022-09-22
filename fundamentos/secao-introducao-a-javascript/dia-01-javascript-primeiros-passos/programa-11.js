e5ba7e086a6f80ae68b56c3d33478245ee68573d
ricardokuhlkamp <ric.kds@hotmail.com>
2022-09-22 00:22:11 -0300

    corrigido novamente programa 11 - INSS e IR

--- fundamentos/secao-introducao-a-javascript/dia-01-javascript-primeiros-passos/programa-11.js
+++ fundamentos/secao-introducao-a-javascript/dia-01-javascript-primeiros-passos/programa-11.js
@@ -3,63 +3,69 @@ Utilize if/else para fazer um programa que, dado um salário bruto, calcule o l�
 Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR.
  */
 
-const salarioBruto = 2500;
+const salBruto = 3000;
+if (salBruto <= 1556.94) {
+  const alqINSS = salBruto * 0.08;
+  const salBase = salBruto - alqINSS;  
+  const alqIR = 0;
+  const parcIR = 0;
+  const valIR = alqIR * salBase - parcIR;
+  const salLiq = salBase - valIR;
+  console.log("1-Resultado: R$ ", salLiq);
+} else if (salBruto >= 1556.95 && salBruto < 2594.93) {
+  const alqINSS = salBruto * 0.09;
+  const salBase = salBruto - alqINSS;
+  if (salBase <= 1903.98) {
+    const alqIR = 0;
+    const parcIR = 0;
+    const valIR = alqIR * salBase - parcIR;
+    const salLiq = salBase - valIR;
+    console.log("2-Resultado: R$ ", salLiq);
+  } else if (salBase > 1903.98 && salBase <= 2826.65) {
+    const alqIR = 0.075;
+    const parcIR = 142.80;
+    const valIR = alqIR * salBase - parcIR;
+    const salLiq = salBase - valIR;
+    console.log("3-Resultado: R$ ", salLiq);
+  }
+} else if (salBruto >= 2594.93 && salBruto < 5189.83) {
+  const alqINSS = salBruto * 0.11;
+  const salBase = salBruto - alqINSS;
+  console.log(salBase)
+  if (salBase > 2826.65 && salBase < 3751.06) {
+    const alqIR = 0.15;
+    const parcIR = 354.80;
+    const valIR = alqIR * salBase - parcIR;
+    const salLiq = salBase - valIR;
+    console.log("4-Resultado: R$ ", salLiq);
+  } else if (salBase >= 3751.06 && salBase < 4664.69) {
+    const alqIR = 0.225;
+    const parcIR = 636.13;
+    const valIR = alqIR * salBase - parcIR;
+    const salLiq = salBase - valIR;
+    console.log("5-Resultado: R$ ", salLiq);  
+  } else if (salBase > 4664.68) {
+    const alqIR = 0.275;
+    const parcIR = 869.36;
+    const valIR = alqIR * salBase - parcIR;
+    const salLiq = salBase - valIR;
+    console.log("5-Resultado: R$ ", salLiq);  
+  } else if (salBase > 1903.98 && salBase <= 2826.65) {
+    const alqIR = 0.075;
+    const parcIR = 142.80;
+    const valIR = alqIR * salBase - parcIR;
+    const salLiq = salBase - valIR;
+    console.log("6-Resultado: R$ ", salLiq);
+  }
 
-if (salarioBruto <= 1556.94) {
-  const valorAliquotaINSS = salarioBruto*0.08;
-  const valorAliquotaIR = 0;
-  const parcelaIR = 0;
-  const salarioLiquido = salarioBruto - valorAliquotaINSS - valorAliquotaIR - parcelaIR;
-  console.log("1-Salário líquido: R$ ", salarioLiquido)
-} else if (salarioBruto >= 1556.95 && salarioBruto < 2594.93) {
-  if (salarioBruto >= 1903.99 && salarioBruto < 2826.66) {
-    const valorAliquotaINSS = salarioBruto*0.09;
-    const valorAliquotaIR = salarioBruto*0.075;
-    const parcelaIR = 142.80;
-    const salarioLiquido = salarioBruto - valorAliquotaINSS - valorAliquotaIR - parcelaIR;
-    console.log("2-Salário líquido: R$ ", salarioLiquido)
-  } else {
-    const valorAliquotaINSS = salarioBruto*0.09;
-    const valorAliquotaIR = 0;
-    const parcelaIR = 0;
-    const salarioLiquido = salarioBruto - valorAliquotaINSS - valorAliquotaIR - parcelaIR;
-    console.log("3-Salário líquido: R$ ", salarioLiquido)
+} else if (salBruto > 5189.92) {
+  const alqINSS = 570.88;
+  const salBase = salBruto - alqINSS;
+  if (salBase > 4664.68) {
+    const alqIR = 0.275;
+    const parcIR = 869.36;
+    const valIR = alqIR * salBase - parcIR;
+    const salLiq = salBase - valIR;
+    console.log("7-Resultado: R$ ", salLiq); 
   }
-  
-} else if (salarioBruto >= 2594.93 && salarioBruto < 5189.83) {
-  if (salarioBruto >= 2826.66 && salarioBruto < 3751.06) {
-    const valorAliquotaINSS = salarioBruto*0.11;
-    const valorAliquotaIR = salarioBruto * 0.15;
-    const parcelaIR = 354.80;
-    const salarioLiquido = salarioBruto - valorAliquotaINSS - valorAliquotaIR - parcelaIR;
-    console.log("4-Salário líquido: R$ ", salarioLiquido)
-  } else if (salarioBruto < 2826.66) {
-    const valorAliquotaINSS = salarioBruto*0.11;
-    const valorAliquotaIR = salarioBruto * 0.075;
-    const parcelaIR = 142.80;
-    const salarioLiquido = salarioBruto - valorAliquotaINSS - valorAliquotaIR;
-    console.log("5-Salário líquido: R$ ", salarioLiquido)
-  } else if (salarioBruto >= 3751.06 && salarioBruto < 4664.69) {
-  const valorAliquotaINSS = salarioBruto*0.11;
-  const valorAliquotaIR = salarioBruto * 0.225;
-  const parcelaIR = 636.13;
-  const salarioLiquido = salarioBruto - valorAliquotaINSS - valorAliquotaIR;
-  console.log("6-Salário líquido: R$ ", salarioLiquido);
-  } else if (salarioBruto > 4664.68) {
-    const valorAliquotaINSS = salarioBruto * 0.11;
-  const valorAliquotaIR = salarioBruto * 0.275;
-  const parcelaIR = 869.36;
-  const salarioLiquido = salarioBruto - valorAliquotaINSS - valorAliquotaIR - parcelaIR;
-  console.log("7-Salário líquido: R$ ", salarioLiquido);
-  }
-} else if (salarioBruto >5189.82) {
-  if (salarioBruto >= 3751.06 && salarioBruto < 4664.69) {
-    const valorAliquotaINSS = 570.88;
-    const valorAliquotaIR = salarioBruto * 0.275;
-    const parcelaIR = 869.36;
-    const salarioLiquido = salarioBruto - valorAliquotaINSS - valorAliquotaIR - parcelaIR;
-    console.log("8-Salário líquido: R$ ", salarioLiquido)
-  }  
 }
-
-
