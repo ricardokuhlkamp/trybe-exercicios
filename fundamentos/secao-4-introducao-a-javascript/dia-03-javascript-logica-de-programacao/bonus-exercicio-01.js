794b4342420300345f5fa503b620b9e93f49f7bf
ricardokuhlkamp <ric.kds@hotmail.com>
2022-09-23 19:57:59 -0300

    exercicio bonus 01 resolvido

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-03-javascript-logica-de-programacao/bonus-exercicio-01.js
@@ -0,0 +1,8 @@
+//Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+
+let resultado = "";
+
+for (let index = 0; index < 5; index+=1) {
+  resultado+="*";
+  console.log(resultado);
+}
