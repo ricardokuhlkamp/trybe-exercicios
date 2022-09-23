d93e0ca41377f8811670e843b30398f67728608c
ricardokuhlkamp <ric.kds@hotmail.com>
2022-09-23 18:22:30 -0300

    exercicio 01 resolvido

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-03-javascript-logica-de-programacao/exercicio-01.js
@@ -0,0 +1,8 @@
+//Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
+
+let resultado = 1;
+for (let index = 10; index > 0; index -= 1) {  
+  resultado = resultado * index;
+}
+
+console.log(resultado)
