3ef2fc089442b91567a0fd749b0c8a09dee4d0d6
ricardokuhlkamp <ric.kds@hotmail.com>
2022-09-24 20:51:16 -0300

    exercicio 03 resolvido

--- fundamentos/secao-4-introducao-a-javascript/dia-03-javascript-logica-de-programacao/exercicio-02.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-03-javascript-logica-de-programacao/exercicio-02.js
@@ -1,3 +1,5 @@
+//Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
 let word = "tryber";
 let palavra = "";
 for (let index = (word.length) -1; index > -1; index-=1) {  
