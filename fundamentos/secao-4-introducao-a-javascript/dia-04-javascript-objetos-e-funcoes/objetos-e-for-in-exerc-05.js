de5fa14c12daefa4a988e2e53c381996bc0982e7
ricardokuhlkamp <ric.kds@hotmail.com>
2022-09-26 20:45:30 -0300

    exercício 05 de objetos e for/in resolvido

--- fundamentos/secao-4-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/objetos-e-for-in-exerc-05.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/objetos-e-for-in-exerc-05.js
@@ -8,3 +8,24 @@ let info = {
 //Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
 
 info['recorrente'] = "Sim";
+
+//Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let properties in info) {
+  if (
+    properties === 'recorrente' &&
+    info[properties] === 'Sim' &&
+    info2[properties] === 'Sim'
+  ) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[properties] + ' e ' + info2[properties]);
+  }
+}
