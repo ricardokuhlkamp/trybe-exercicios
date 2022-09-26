1013db3a51a7cc3e66c862b66ab3b5367437904b
ricardokuhlkamp <ric.kds@hotmail.com>
2022-09-26 20:26:25 -0300

    comitando alterações do exercicio

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/objetos-e-for-in-exerc-05.js
@@ -0,0 +1,10 @@
+//Usando o objeto abaixo, faça os exercícios a seguir:
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+//Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
+
+info['recorrente'] = "Sim";
