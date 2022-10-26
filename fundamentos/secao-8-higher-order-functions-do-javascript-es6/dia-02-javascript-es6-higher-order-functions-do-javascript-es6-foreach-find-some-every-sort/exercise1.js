e62f0dcd1f0098d2ae4c40b28aaa537dd9e327ac
ricardokuhlkamp <ric.kds@hotmail.com>
2022-10-26 09:13:00 -0300

    exercicios resolvidos

--- fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-do-javascript-es6-foreach-find-some-every-sort/exercise1.js
+++ fundamentos/secao-8-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-do-javascript-es6-foreach-find-some-every-sort/exercise1.js
@@ -130,11 +130,6 @@ function someBookWasReleaseOnThe80s(arr) {
 
 // Faça uma função que retorne true, caso nenhum author tenha nascido no mesmo ano, e false, caso contrário.
 
-function authorUnique() {
-  return books.every((book) =>
-    !books.some((bookSome) =>
-      (bookSome.author.birthYear === book.author.birthYear)
-      && (bookSome.author.name !== book.author.name)));
-}
-
-console.log(authorUnique())
+const authorUnique = () => books.every((book) => books.
+  some((someBook) => ((someBook.author.birthYear === book.author.birthYear) && (someBook.author.name !== book.author.name)) ? true : false));
+console.log(authorUnique());
